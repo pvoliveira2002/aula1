991a1891e8344149c18226b8202f5ee2b5fb7c0e
pvoliveira2002 <pvoliveira2002@gmail.com>
2021-11-29 20:30:16 -0200

    versão 0.0.4

--- olamundo.php
+++ olamundo.php
@@ -4,4 +4,4 @@ echo"Ola,Mund3o!, Me chamo malaquias";
 echo"Ola,Mund3o!, Me chamo johnwick" ;
 echo"Ola,Mund3o!, me chamo kingman";
 echo"Ola,Mund3o!, jamal malik";
-echo"Ola,Mund3o!, dororo";
+echo"Ola,Mund3o!, dorororo";
